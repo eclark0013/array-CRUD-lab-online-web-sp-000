2d4035c3428774166fe0642101a48947398bbfd4
Eric Clark <eclark0013@gmail.com>
2019-06-10 14:48:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,15 +19,15 @@ array.push
 end
 
 def remove_element_from_start_of_array(array)
-
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+array.first
 end
 
 def retrieve_last_element_from_array(array)
